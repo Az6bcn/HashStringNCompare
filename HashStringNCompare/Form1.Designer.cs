e1c4a42d6edcee0e88cd8b4c8a3fce3a8c27f8d1
Azeez <az6bcn@gmail.com>
2017-08-12 21:11:45 +0200

    Agregar archivos de proyecto.

---
+++ HashStringNCompare/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HashStringNCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stringToHash = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textStringToHash = new System.Windows.Forms.TextBox();
+            this.textResultHash = new System.Windows.Forms.TextBox();
+            this.btnToHash = new System.Windows.Forms.Button();
+            this.btnCompareToHash = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textHashCompare = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // stringToHash
+            // 
+            this.stringToHash.AutoSize = true;
+            this.stringToHash.Location = new System.Drawing.Point(41, 82);
+            this.stringToHash.Name = "stringToHash";
+            this.stringToHash.Size = new System.Drawing.Size(70, 13);
+            this.stringToHash.TabIndex = 0;
+            this.stringToHash.Text = "stringToHash";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "resultHash";
+            // 
+            // textStringToHash
+            // 
+            this.textStringToHash.Location = new System.Drawing.Point(113, 82);
+            this.textStringToHash.Name = "textStringToHash";
+            this.textStringToHash.Size = new System.Drawing.Size(256, 20);
+            this.textStringToHash.TabIndex = 2;
+            // 
+            // textResultHash
+            // 
+            this.textResultHash.Location = new System.Drawing.Point(113, 138);
+            this.textResultHash.Name = "textResultHash";
+            this.textResultHash.Size = new System.Drawing.Size(256, 20);
+            this.textResultHash.TabIndex = 3;
+            // 
+            // btnToHash
+            // 
+            this.btnToHash.Location = new System.Drawing.Point(141, 197);
+            this.btnToHash.Name = "btnToHash";
+            this.btnToHash.Size = new System.Drawing.Size(75, 23);
+            this.btnToHash.TabIndex = 4;
+            this.btnToHash.Text = "Hash";
+            this.btnToHash.UseVisualStyleBackColor = true;
+            this.btnToHash.Click += new System.EventHandler(this.btnToHash_Click);
+            // 
+            // btnCompareToHash
+            // 
+            this.btnCompareToHash.Location = new System.Drawing.Point(141, 359);
+            this.btnCompareToHash.Name = "btnCompareToHash";
+            this.btnCompareToHash.Size = new System.Drawing.Size(85, 23);
+            this.btnCompareToHash.TabIndex = 5;
+            this.btnCompareToHash.Text = "CompareHash";
+            this.btnCompareToHash.UseVisualStyleBackColor = true;
+            this.btnCompareToHash.Click += new System.EventHandler(this.btnCompareToHash_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "newStringToHash";
+            // 
+            // textHashCompare
+            // 
+            this.textHashCompare.Location = new System.Drawing.Point(141, 313);
+            this.textHashCompare.Name = "textHashCompare";
+            this.textHashCompare.Size = new System.Drawing.Size(256, 20);
+            this.textHashCompare.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 411);
+            this.Controls.Add(this.textHashCompare);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCompareToHash);
+            this.Controls.Add(this.btnToHash);
+            this.Controls.Add(this.textResultHash);
+            this.Controls.Add(this.textStringToHash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stringToHash);
+            this.Name = "Form1";
+            this.Text = "b";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stringToHash;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textStringToHash;
+        private System.Windows.Forms.TextBox textResultHash;
+        private System.Windows.Forms.Button btnToHash;
+        private System.Windows.Forms.Button btnCompareToHash;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textHashCompare;
+    }
+}
+
---
+++ HashStringNCompare/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace HashStringNCompare
+{
+    public partial class Form1 : Form
+    {
+        private string hashed_result;
+        private string hashed_result2;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToHash_Click(object sender, EventArgs e)
+        {
+            //get the input string to be Hashed
+            string txtToHash = textStringToHash.Text;
+            //Hash this data 
+            HashClass H = new HashClass();
+            hashed_result = H.MD5_Encode(txtToHash);
+            //show the result in the form
+            textResultHash.Text = hashed_result;
+
+
+        }
+
+        private void btnCompareToHash_Click(object sender, EventArgs e)
+        {
+            //get the input string to be Hashed
+            string txtToHash = textStringToHash.Text;
+            //Hash this data 
+            HashClass H = new HashClass();
+            hashed_result = H.MD5_Encode(txtToHash);
+            //get the input string to be Hashed
+            string txtToHash2 = textHashCompare.Text;
+            hashed_result2 = H.MD5_Encode(txtToHash2);
+
+            //Compare result
+            bool res = this.hashed_result.Equals(this.hashed_result2);
+            if (res)
+            {
+                MessageBox.Show("Son Iguales !!! " + this.hashed_result + "==" + this.hashed_result2);
+            }
+            else MessageBox.Show("No Son Iguales!!!!");
+        }
+    }
+    public class HashClass
+    {
+        //method to Hash--> returns a string which is the Hash itself
+        public string MD5_Encode (string stringToHash)
+        {
+            //MD5 Object
+            MD5 md5Hash = MD5.Create();
+            //Convert the input string into a Byte Array and compute the HASH
+            byte[] hash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(stringToHash));
+
+            //Convert the Hashed Byte Array to string using StringBuilder
+            StringBuilder sBuilder = new StringBuilder();
+
+            //Iterate the Array of Hashed String and convert them to Hex string 
+            for(int i= 0; i < hash.Length; i++)
+            {
+                sBuilder.Append(hash[i].ToString("X2"));
+            }
+
+            //return the Hex string
+            return sBuilder.ToString();
+        }
+    }
+    }
